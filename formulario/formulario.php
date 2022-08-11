fdc23fe83807d998d756464a887ac3699b4b577d
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-08-11 14:33:59 -0500

    Exibindo erros de validação

--- formulario/formulario.php
+++ formulario/formulario.php
@@ -5,36 +5,38 @@
 
 <?php
 if(count($_POST) > 0){
+    $erros = [];
+
     // isset($_POST['nome'])
     if(!filter_input(INPUT_POST, "nome")){
-        echo 'Nome é obrigatório', '<br>';
+        $erros['nome'] = 'Nome é obrigatório';
     }
 
     if(filter_input(INPUT_POST, "nascimento")){
         $data = DateTime::createFromFormat('d/m/Y', $_POST['nascimento']);
         if(!$data){
-            echo 'Data deve estar no padrão dd/mm/aaaa', '<br>';
+            $erros['nascimento'] = 'Data deve estar no padrão dd/mm/aaaa';
         }
     }
 
     if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
-        echo 'Email inválido', '<br>';
+        $erros['email'] = 'Email inválido';
     }
 
     if(!filter_var($_POST['site'], FILTER_VALIDATE_URL)){
-        echo 'Site inválido', '<br>';
+        $erros['site'] = 'Site inválido';
     }
 
     $filhosConfig = [
         "options" => ["min_range" => 0, "max_range" => 20]
     ];
     if(!filter_var($_POST['filhos'], FILTER_VALIDATE_INT, $filhosConfig && $_POST['filhos'] != 0)){
-        echo 'Quantidade de filhos inválida', '<br>';
+        $erros['filhos'] = 'Quantidade de filhos inválida';
     }
 
     $salarioConfig = ['options' => ['decimal' => ',']];
     if(!filter_var($_POST['salario'], FILTER_VALIDATE_FLOAT, $salarioConfig)){
-        echo 'Salário inválido', '<br>';
+        $erros['salario'] = 'Salário inválido';
     }
 }
 ?>
