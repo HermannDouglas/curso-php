aedde1ce277eefcbedaf4c9b33dfb6c2d4d230d6
Hermann Douglas Bezerra de Almeida <hermann.hdba@gmail.com>
2022-08-23 17:59:15 -0500

    Implementando consulta_pdo

--- db/consultar_pdo.php
+++ db/consultar_pdo.php
@@ -5,7 +5,11 @@ require_once "conexao_pdo.php";
 
 $conexao = novaConexao();
 $sql = "SELECT * FROM cadastro";
-$resultado = $conexao->query($sql)->fetchAll();
+// PDO::FETCH_NUM
+// PDO::FETCH_ASSOC
+// PDO::FETCH_CLASS
+// PDO::FETCH_BOTH
+$resultado = $conexao->query($sql)->fetchAll(PDO::FETCH_NUM);
 // Continuar...
 print_r($resultado);
 
